f7b61a2cf3b15d778d00e233efe9c412d3a082b5
apaigude <you@example.com>
2021-07-22 16:40:02 -0500

    forcefull build failed

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,7 +9,7 @@ public class App {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World")
 		System.out.println("Hello Hello");
 		System.out.println("Welcome to Java");
 	}
